cbd4385b60262c575b0a2225c1a6717adcdb5b10
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-14 19:49:22 +0530

    added slug based routing

---
+++ src/app/documentation/[...slugs]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Docs = ({params}: {params: {slugs: string[]}}) => {
+  const {slugs} = params;
+  return (
+    <div>
+      This is the documentation home page with
+      {slugs.length === 1 && <h1>Welcome to the Concept {slugs[0]} page</h1>}
+      {slugs.length === 2 && <h1>Welcome to the Example {slugs[1]} page of concept{slugs[0]}</h1>}
+    </div>
+  )
+}
+
+export default Docs
