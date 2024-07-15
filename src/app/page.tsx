d5aafe9facf4ab53f369b882848d46dd73c5f712
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-15 20:33:37 +0530

    added link tags to the root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-[80vh] flex-col items-center justify-center p-24">
+      <ul>
+        <li><Link href={"/about"} >About</Link></li>
+        <li><Link href={"/blog"} >blog</Link></li>
+        <li><Link href={"/documentation"} >Docs</Link></li>
+      </ul>
       <h1>Hello world</h1>
     </main>
   );
