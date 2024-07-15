b0c1bfd279afd894fdaade3a81f2a30277c48170
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-15 21:09:56 +0530

    added template file for the auth psuedo path

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -1,4 +1,4 @@
-export default function RootLayout({
+export default function AuthTemplate({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -7,6 +7,7 @@ export default function RootLayout({
     <div>
         <div className="w-screen h-[10vh] bg-purple-100 text-black">
             <h1>This is the authentication heada</h1>
+            <input type="text" placeholder="enter something" className="transition duration-300 ease-in focus:bg-black focus:text-white text-black bg-transparent border border-black p-2 m-2" />
         </div>
         {children}
         <div className="w-screen h-[10vh] bg-amber-100 text-black">
