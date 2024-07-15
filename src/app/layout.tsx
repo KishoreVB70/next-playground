fa7527fdb8e9088a380bb1db1c0be936dbc96be1
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-15 20:30:36 +0530

    added title object to the root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,10 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Kishore VB",
+  title: {
+    default: "Kishore VB",
+    template: ""
+  },
   description: "Portfolio site of Kishore V B",
 };
 
