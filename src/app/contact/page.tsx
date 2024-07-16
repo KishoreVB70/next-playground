b7dc7640986c4ef2b49c1ba1839238457aea7a61
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-16 14:59:04 +0530

    added contact path

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ContactIcons = () => {
+  return (
+    <div>
+        <div>Github</div>
+        <div>Youtube</div>
+        <div>Twitter</div>
+        <div>Linked in maybe</div>
+    </div>
+  )
+}
+
+export default ContactIcons
