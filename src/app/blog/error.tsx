f448c9ece76a577bdb6c162075bf68181551c74d
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-16 07:28:52 +0530

    added reset method for error handling in blog

--- src/app/blog/error.tsx
+++ src/app/blog/error.tsx
@@ -1,8 +1,18 @@
+"use client";
 import React from 'react'
 
-const ErrorBoundary = ({error}: {error: Error}) => {
+type prop = {
+  error: Error,
+  reset: () => void
+}
+
+const ErrorBoundary = ({error, reset}: prop) => {
   return (
-    <div>ErrorBoundary in blog page - {error.message}</div>
+    <div>
+        Why error in blog page
+        {error.message}
+        <button onClick={reset} className="ease-in hover:-translate-y-1 hover:border-red-600 transition duration-300 hover:bg-white hover:text-black border border-white px-3 py-1 text-white" >Reload page</button>
+    </div>
   )
 }
 
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
-
+"use client";
 const Blog = () => {
+  // const botError = () => {
+  //   console.log("fuck next");
+  //   throw new Error("fuck next");
+  // }
+
+  const getRandom = () => {
+    return Math.floor(Math.random() * 2);
+  }
+
+  const randomValue = getRandom();
+
+  if(randomValue === 1) {
+    throw new Error("Value is 1 brotha")
+  }
+
   return (
-    <div>Blog</div>
+      <div className='flex justify-center' >
+        Blog
+        {/* <button onClick={botError} className="ease-in hover:-translate-y-1 hover:border-red-600 transition duration-300 hover:bg-white hover:text-black border border-white px-3 py-1 text-white" >Throw error</button> */}
+      </div>
+
   )
 }
 
