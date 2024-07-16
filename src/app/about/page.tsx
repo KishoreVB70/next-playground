f4aa3e6fd660256df902c1b5e87a8f45fbc25f74
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-16 16:27:07 +0530

    implemented fetch in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,12 @@
-"use client";
-import { useRouter } from 'next/navigation';
 import React from 'react'
+export const metadata = {title: "about kishore"};
 
-// export const metadata = {title: "about kishore"};
-const About = () => {
-  const router = useRouter();
+const About = async() => {
+  const _data = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await _data.json();
+  console.log(data);
   return (
     <div className='h-[80vh] flex justify-center items-center' >
-      <button onClick={() => router.push("/login")} className="ease-in hover:-translate-y-1 hover:border-red-600 transition duration-300 hover:bg-white hover:text-black border border-white px-3 py-1 text-white" >Login</button>
     </div>
   )
 }
