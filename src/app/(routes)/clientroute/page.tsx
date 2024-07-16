3bebbcaae2dcd7d6ed2dee54be506405fca2ccb2
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-16 16:24:05 +0530

    moved login button to the clientROute

--- src/app/(routes)/clientroute/page.tsx
+++ src/app/(routes)/clientroute/page.tsx
@@ -1,13 +1,17 @@
 "use client"
 import { useTheme } from '@/components/ThemeProvider'
-import { serverFunction } from '@/utils/serverutils'
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const ClientRoute = () => {
   const theme = useTheme();
-    // const value = serverFunction();
+  const router = useRouter();
+
   return (
-    <div style={{color: theme.primaryColor}} >ClientRoute</div>
+    <div style={{color: theme.primaryColor}} >
+      <button onClick={() => router.push("/login")} className="ease-in hover:-translate-y-1 hover:border-red-600 transition duration-300 hover:bg-white hover:text-black border border-white px-3 py-1 text-white" >Login</button>
+    </div>
+
   )
 }
 
